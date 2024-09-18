30f89b18723e99daecd65cc31f920a099e478111
Joshua Comeau <joshwcomeau@gmail.com>
2024-09-18 10:25:31 -0400

    Create repro using Pages router

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import styles from './Button.module.css';
+
+function Button({ children }) {
+  return <button className={styles.Button}>{children}</button>;
+}
+
+export default Button;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,11 @@
-import styles from './index.module.css';
+import styles from './about.module.css';
 
 import Button from '../components/Button';
 
 export default function Home() {
   return (
     <>
-      <h1 className={styles.Home}>Homepage</h1>
+      <h1 className={styles.About}>About Page</h1>
       <Button>Example component</Button>
     </>
   );
